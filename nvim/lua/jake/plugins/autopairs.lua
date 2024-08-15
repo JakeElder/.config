1249e8bd042875b9d0a28742785f8a53c53d0fb2
Jake Elder <jake@2pm.io>
2024-08-15 11:34:45 +0700

    fix: autopairs

--- nvim/lua/jake/plugins/autopairs.lua
+++ nvim/lua/jake/plugins/autopairs.lua
@@ -1,15 +1,24 @@
 return {
-  "windwp/nvim-autopairs",
-  event = { "InsertEnter" },
-  config = function()
-    local autopairs = require("nvim-autopairs")
-    autopairs.setup({
-      check_ts = true,
-      ts_config = {
-        lua = { "string" },
-        javascript = { "template_string" },
-        java = false,
-      },
-    })
-  end,
+	"windwp/nvim-autopairs",
+	event = { "InsertEnter" },
+	config = function()
+		local autopairs = require("nvim-autopairs")
+		autopairs.setup({
+			check_ts = true,
+			ts_config = {
+				lua = { "string" },
+				javascript = { "template_string" },
+				java = false,
+			},
+		})
+
+		-- import nvim-autopairs completion functionality
+		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+
+		-- import nvim-cmp plugin (completions plugin)
+		local cmp = require("cmp")
+
+		-- make autopairs and completion work together
+		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+	end,
 }
--- nvim/lua/jake/plugins/treesitter.lua
+++ nvim/lua/jake/plugins/treesitter.lua
@@ -1,50 +1,54 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
-  build = ":TSUpdate",
-  dependencies = {
-    "windwp/nvim-ts-autotag",
-  },
-  config = function()
-    local treesitter = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPre", "BufNewFile" },
+	build = ":TSUpdate",
+	dependencies = {
+		"windwp/nvim-ts-autotag",
+	},
+	config = function()
+		local treesitter = require("nvim-treesitter.configs")
 
-    treesitter.setup({ 
-      highlight = {
-        enable = true,
-      },
-      indent = { enable = true },
-      autotag = {
-        enable = true,
-      },
-      ensure_installed = {
-        "json",
-        "javascript",
-        "typescript",
-        "tsx",
-        "yaml",
-        "html",
-        "css",
-        "prisma",
-        "markdown",
-        "markdown_inline",
-        "graphql",
-        "bash",
-        "lua",
-        "vim",
-        "dockerfile",
-        "gitignore",
-        "query",
-        "vimdoc",
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<leader>a",
-          node_incremental = "<leader>a",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
-    })
-  end,
+		require("nvim-ts-autotag").setup({
+			enable = true,
+			filetypes = { "html", "xml", "tsx" },
+		})
+
+		treesitter.setup({
+			highlight = {
+				enable = true,
+			},
+			indent = { enable = true },
+			ensure_installed = {
+				"json",
+				"javascript",
+				"typescript",
+				"tsx",
+				"yaml",
+				"html",
+				"css",
+				"prisma",
+				"markdown",
+				"markdown_inline",
+				"svelte",
+				"graphql",
+				"bash",
+				"lua",
+				"vim",
+				"dockerfile",
+				"gitignore",
+				"query",
+				"vimdoc",
+				"c",
+			},
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<leader>a",
+					node_incremental = "<leader>a",
+					scope_incremental = false,
+					node_decremental = "<bs>",
+				},
+			},
+		})
+	end,
 }
