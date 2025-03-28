ec8dcffaffb96689a4e7cd4208e9d7002505bf59
Jake Elder <jake@2pm.io>
2025-03-28 11:00:55 +0700

    feat: stuff

---
+++ nvim/lua/jake/plugins/codecompanion.lua
@@ -0,0 +1,21 @@
+return {
+	"olimorris/codecompanion.nvim",
+	config = true,
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	opts = {
+		strategies = {
+			chat = {
+				adapter = "anthropic",
+			},
+			inline = {
+				adapter = "anthropic",
+			},
+			cmd = {
+				adapter = "anthropic",
+			},
+		},
+	},
+}
